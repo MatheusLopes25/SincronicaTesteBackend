7c3c5c6343dc347f319c07f2275a1a86a2a4064d
matheus lopes <matheuslopes12@hotmail.com>
2025-04-07 22:30:58 -0300

    Ajuste em ProdutoSeeders.php

--- database/seeders/ProdutoSeeder.php
+++ database/seeders/ProdutoSeeder.php
@@ -20,20 +20,19 @@ class ProdutoSeeder extends Seeder
             'preco' => 1999.99,
             'descricao' => "Smartphone com 4gb de ram, 250 de armazenamento",
             'categoria_id' => $categoria->id
-        ],
-        [
+        ]);
+        Produto::create([
             'nome' => 'Smart Tv Sansung',
             'preco' => 5000.00,
             'descricao' => "Smart Tv Sansung",
             'categoria_id' => $categoria->id
-        ], 
-        [
+        ]); 
+        Produto::create([
             'nome' => 'Apple notebook MacBook Air',
             'preco' => 8000.00,
             'descricao' => "notebook de 13 polegadas, Processador M1 da Apple com CPU 8‑core e GPU 7‑core, 8 GB RAM, 256 GB",
             'categoria_id' => $categoria->id
-        ]   
-    );
+        ]);   
         
     }
 }
